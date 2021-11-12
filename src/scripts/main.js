b668bac066ded9eee3f2bbf98520b9303743f7f9
Quazariun <quazariun@gmail.com>
2021-11-12 17:01:02 -0300

    Add resize and normalize

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,20 @@ import * as PIXI from '../libs/pixijs/pixi.mjs'
 
 // Setup inicial
 const app = new PIXI.Application({
+	autoResize: true,
 	backgroundColor: 0xd96200
-	
 });
+
+function resize() {
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+}
 
 document.body.appendChild(app.view);
 
 // idle position
 const frames = [];
-for( let i = 0; i < 8; ++i){
-	frames.push(PIXI.Texture.from(`./src/sprites/water_goddess/01_idle/idle_${i+1}.png`));	
+for (let i = 0; i < 8; ++i) {
+	frames.push(PIXI.Texture.from(`./src/sprites/water_goddess/01_idle/idle_${i + 1}.png`));
 }
 
 const anim = new PIXI.AnimatedSprite(frames);
@@ -19,7 +23,13 @@ anim.play();
 
 app.stage.addChild(anim);
 
+
+window.addEventListener('resize', resize);
+
 // Core Loop
 app.ticker.add(() => {
 	anim.rotation += 0;
 });
+
+resize();
+
